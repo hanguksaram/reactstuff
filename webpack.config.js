d6f258dbdbb7c86b791ff00f383bf50b599bd0bb
hanguksaram <artur.pozhidaev@gmail.com>
2018-06-26 10:48:19 +0700

    Setup devDependencies

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = (env) => {
     entry: ['react-dates/initialize',
   'react-dates/lib/css/_datepicker.css', './src/app.js'],
   output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
 
   },
@@ -52,7 +52,8 @@ module.exports = (env) => {
   devtool: isProduction ? 'source-map' : 'inline-source-map',
   devServer:{
     contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
+    historyApiFallback: true,
+    publicPath: '/dist/'
   } 
   }
 }
